9cdae0cc23be1c89d18d45b6d9bec3112ff03395
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-30 18:31:42 +0600

    12. Displaying the Autocomplete

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,16 @@
-it('works!', () => {
-    
+it("Shows an autocomplete !", () => {
+  createAutoComplete({
+    root: document.querySelector("#target"),
+    fetchData() {
+      return [
+        { Title: "The Avengers" },
+        { Title: "Avengers End Game" },
+        { Title: "Thor" },
+      ];
+    },
+
+    renderOption(movie) {
+      return movie.Title;
+    },
+  });
 });
